80933649d9069786c38cec08617a5a563e903171
ll <774365046@qq.com>
2017-07-06 22:03:23 +0800

    創建商品模型類

--- ec/goods/models.py
+++ ec/goods/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=20)        # 商品名
+    price = models.FloatField()                   # 商品價格
+    brief = models.CharField(max_length=100)      # 商品簡介
+    detail = models.CharField(max_length=500)     # 商品詳情
+    sold = models.IntegerField(default=0)         # 商品的銷量（重量）
+    origin = models.CharField(max_length=50)      # 商品的圖片地址
+
--- ec/goods/views.py
+++ ec/goods/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'goods/index.html')
+    context = {'title': '首頁', 's': '0'}
+    return render(request, 'goods/index.html', context)
+
+def fruit(request):
+    context = {'title': '水果', 'id': '1'}
+    return render(request, 'goods/list.html', context)
