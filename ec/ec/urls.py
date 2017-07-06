31c39a44a3dfe655c39c530bd3b987887f1255dd
ll <774365046@qq.com>
2017-07-06 18:09:44 +0800

    修改

--- ec/ec/urls.py
+++ ec/ec/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('user.urls')),
+    url(r'^user', include('user.urls')),
+    url(r'^goods', include('goods.urls')),
 ]
